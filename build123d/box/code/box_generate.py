341d2504ac15cdec8d1b9bef6339259e68178363
Abhay Gupta <ab18gu@gmail.com>
2025-09-09 16:17:43 -0700

    Add ocp cad viewer package to box

--- build123d/box/code/box_generate.py
+++ build123d/box/code/box_generate.py
@@ -1,13 +1,13 @@
 from build123d import *
-# from ocp_vscode import show
+from ocp_vscode import show
 
 with BuildPart() as bp:
     Box(20, 30, 10) # length, width, height
     fillet(objects=bp.edges().filter_by(Axis.Z), radius=2)
 
 # Show in OCP CAD viewer
-# show(bp)
+show(bp)
 
 # Export using build123d's export functionality
-export_stl(bp.part, "../generated-cad/rounded_box.stl")
-export_step(bp.part, "../generated-cad/rounded_box.step")
+# export_stl(bp.part, "../generated-cad/rounded_box.stl")
+# export_step(bp.part, "../generated-cad/rounded_box.step")

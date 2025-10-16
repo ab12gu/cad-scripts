9bb3cd6205b1c2fec69cadd3cf4f8799cb16a6ee
Abhay Gupta <ab18gu@gmail.com>
2025-10-16 13:57:08 -0700

    Cuts finished for hinge

--- build123d/table-ratcheting-hinge/code/table-ratcheting-hinge.py
+++ build123d/table-ratcheting-hinge/code/table-ratcheting-hinge.py
@@ -8,23 +8,53 @@
 from build123d import *
 from ocp_vscode import show
 
+inch = 25.4
 length = 256
-thickness = 50.8
-width = 101.6
+thickness = inch*2
+width = inch*4
 
 with BuildPart() as hinge_p:
     with BuildSketch() as hinge_sk:
+
         with BuildLine() as hinge_ln:
             l1 = Line((0,0), (length, 0))
             l2 = Line((length,0), (length, width))
             l3 = Line((length,width), (0, width))
             l4 = Line((0,width), (0, 0))
         make_face()
+
+        with Locations((length-2*inch,inch)):
+            Circle(inch/4,mode=Mode.SUBTRACT)
+
+        with Locations((2*inch,inch)):
+            Circle(inch/4,mode=Mode.SUBTRACT)
+        
+        for i in range(3):
+            with Locations((3*inch*i+2*inch,width-inch)):
+                Circle(inch,mode=Mode.SUBTRACT)
+            with Locations((3*inch*i+2*inch,width)):
+                Rectangle(2*inch,2*inch,mode=Mode.SUBTRACT)
     extrude(amount=thickness)
+
+    with BuildSketch(Plane.XZ) as triangle_sk:
+        with Locations((inch/4,inch)):
+            RegularPolygon(radius=inch/2, side_count=3)
+    extrude(amount=-thickness, mode=Mode.SUBTRACT)
+
+    with BuildSketch(Plane.XZ) as triangle_sk:
+        with Locations((inch/4+length,inch)):
+            RegularPolygon(radius=inch/2, side_count=3)
+    extrude(amount=-thickness)
+
+
+
+
+
 
 
 # Show in OCP CAD viewer
 #show(ex4)
+#show(hinge_sk)
 show(hinge_p)
 
 # Export using build123d's export functionality
